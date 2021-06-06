ca1116c00d69553c56c672e435a4ed093b629218
Carlos <carlos@localhost.localdomain>
2021-06-06 17:33:47 -0400

    borrado de algunos comentarios y definición de función.

--- k2tree_operations_parallel.h
+++ k2tree_operations_parallel.h
@@ -1,4 +1,5 @@
 #include "k2tree_operations.h"
 
+ulong ** posByLevel_parallel(MREP * X);
 uint intersectionOperation_parallel(uint l, MREP * A, MREP * B, ulong * pA, ulong * pB, misBits * C);
 MREP * k2tree_intersection_parallel(MREP * repA, MREP * repB);
