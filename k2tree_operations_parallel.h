fc852b3653ffad98fa94cf126102fe189bcb6bb8
Carlos <carlos@localhost.localdomain>
2021-06-06 21:13:56 -0400

    Generar función que sólo implementa estrategia sin paralelizar, para comparar.

--- k2tree_operations_parallel.h
+++ k2tree_operations_parallel.h
@@ -3,3 +3,4 @@
 ulong ** posByLevel_parallel(MREP * X);
 uint intersectionOperation_parallel(uint l, MREP * A, MREP * B, ulong * pA, ulong * pB, misBits * C);
 MREP * k2tree_intersection_parallel(MREP * repA, MREP * repB);
+MREP * k2tree_intersection_estrategia_parallel(MREP * repA, MREP * repB);
